840930141b092ec52dc9e4245a46a5dcd98b7c36
ASC-O <74096576+ASC-O@users.noreply.github.com>
2021-03-28 13:20:29 +0000

    Update to the footer

--- src/layouts/footer.svelte
+++ src/layouts/footer.svelte
@@ -1,20 +1,4 @@
 <footer id="footerbot" class="page-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col l6 s12">
-        <h5 class="white-text">The meme site</h5>
-        <p class="grey-text text-lighten-4">The memes are real.</p>
-      </div>
-      <div class="col l4 offset-l2 s12">
-        <h5 class="white-text">Links</h5>
-        <ul>
-          <li><a class="grey-text text-lighten-3" href="#!">Home</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Memes</a></li>
-          <li><a class="grey-text text-lighten-3" href="#!">Github</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
   <div class="footer-copyright">
     <div class="container">
     © 2021 The Mysterious Developer
